d7de9484237c4b362237d93295cdcac23da43049
Zethson <lukas.heumos@gmail.com>
2019-04-02 16:52:59 +0200

    [FEATURE] new  sponsors 2018 logo and disabled gallery animations #30

--- igem_tuebingen_website/static/assets/js/slider_init.js
+++ igem_tuebingen_website/static/assets/js/slider_init.js
@@ -6,177 +6,177 @@ jssor_1_slider_init = function () {
                         $Zoom: 1,
                         $Easing: {$Zoom: $Jease$.$InCubic, $Opacity: $Jease$.$OutQuad},
                         $Opacity: 2
-                    },
-                    {
-                        $Duration: 1000,
-                        $Zoom: 11,
-                        $SlideOut: true,
-                        $Easing: {$Zoom: $Jease$.$InExpo, $Opacity: $Jease$.$Linear},
-                        $Opacity: 2
-                    },
-                    {
-                        $Duration: 1200,
-                        $Zoom: 1,
-                        $Rotate: 1,
-                        $During: {$Zoom: [0.2, 0.8], $Rotate: [0.2, 0.8]},
-                        $Easing: {$Zoom: $Jease$.$Swing, $Opacity: $Jease$.$Linear, $Rotate: $Jease$.$Swing},
-                        $Opacity: 2,
-                        $Round: {$Rotate: 0.5}
-                    },
-                    {
-                        $Duration: 1000,
-                        $Zoom: 11,
-                        $Rotate: 1,
-                        $SlideOut: true,
-                        $Easing: {$Zoom: $Jease$.$InQuint, $Opacity: $Jease$.$Linear, $Rotate: $Jease$.$InQuint},
-                        $Opacity: 2,
-                        $Round: {$Rotate: 0.8}
-                    },
-                    {
-                        $Duration: 1200,
-                        x: 0.5,
-                        $Cols: 2,
-                        $Zoom: 1,
-                        $Assembly: 2049,
-                        $ChessMode: {$Column: 15},
-                        $Easing: {$Left: $Jease$.$InCubic, $Zoom: $Jease$.$InCubic, $Opacity: $Jease$.$Linear},
-                        $Opacity: 2
-                    },
-                    {
-                        $Duration: 1200,
-                        x: 4,
-                        $Cols: 2,
-                        $Zoom: 11,
-                        $SlideOut: true,
-                        $Assembly: 2049,
-                        $ChessMode: {$Column: 15},
-                        $Easing: {$Left: $Jease$.$InExpo, $Zoom: $Jease$.$InExpo, $Opacity: $Jease$.$Linear},
-                        $Opacity: 2
-                    },
-                    {
-                        $Duration: 1200,
-                        x: 0.6,
-                        $Zoom: 1,
-                        $Rotate: 1,
-                        $During: {$Left: [0.2, 0.8], $Zoom: [0.2, 0.8], $Rotate: [0.2, 0.8]},
-                        $Opacity: 2,
-                        $Round: {$Rotate: 0.5}
-                    },
-                    {
-                        $Duration: 1000,
-                        x: -4,
-                        $Zoom: 11,
-                        $Rotate: 1,
-                        $SlideOut: true,
-                        $Easing: {
-                            $Left: $Jease$.$InQuint,
-                            $Zoom: $Jease$.$InQuart,
-                            $Opacity: $Jease$.$Linear,
-                            $Rotate: $Jease$.$InQuint
-                        },
-                        $Opacity: 2,
-                        $Round: {$Rotate: 0.8}
-                    },
-                    {
-                        $Duration: 1200,
-                        x: -0.6,
-                        $Zoom: 1,
-                        $Rotate: 1,
-                        $During: {$Left: [0.2, 0.8], $Zoom: [0.2, 0.8], $Rotate: [0.2, 0.8]},
-                        $Opacity: 2,
-                        $Round: {$Rotate: 0.5}
-                    },
-                    {
-                        $Duration: 1000,
-                        x: 4,
-                        $Zoom: 11,
-                        $Rotate: 1,
-                        $SlideOut: true,
-                        $Easing: {
-                            $Left: $Jease$.$InQuint,
-                            $Zoom: $Jease$.$InQuart,
-                            $Opacity: $Jease$.$Linear,
-                            $Rotate: $Jease$.$InQuint
-                        },
-                        $Opacity: 2,
-                        $Round: {$Rotate: 0.8}
-                    },
-                    {
-                        $Duration: 1200,
-                        x: 0.5,
-                        y: 0.3,
-                        $Cols: 2,
-                        $Zoom: 1,
-                        $Rotate: 1,
-                        $Assembly: 2049,
-                        $ChessMode: {$Column: 15},
-                        $Easing: {
-                            $Left: $Jease$.$InCubic,
-                            $Top: $Jease$.$InCubic,
-                            $Zoom: $Jease$.$InCubic,
-                            $Opacity: $Jease$.$OutQuad,
-                            $Rotate: $Jease$.$InCubic
-                        },
-                        $Opacity: 2,
-                        $Round: {$Rotate: 0.7}
-                    },
-                    {
-                        $Duration: 1000,
-                        x: 0.5,
-                        y: 0.3,
-                        $Cols: 2,
-                        $Zoom: 1,
-                        $Rotate: 1,
-                        $SlideOut: true,
-                        $Assembly: 2049,
-                        $ChessMode: {$Column: 15},
-                        $Easing: {
-                            $Left: $Jease$.$InExpo,
-                            $Top: $Jease$.$InExpo,
-                            $Zoom: $Jease$.$InExpo,
-                            $Opacity: $Jease$.$Linear,
-                            $Rotate: $Jease$.$InExpo
-                        },
-                        $Opacity: 2,
-                        $Round: {$Rotate: 0.7}
-                    },
-                    {
-                        $Duration: 1200,
-                        x: -4,
-                        y: 2,
-                        $Rows: 2,
-                        $Zoom: 11,
-                        $Rotate: 1,
-                        $Assembly: 2049,
-                        $ChessMode: {$Row: 28},
-                        $Easing: {
-                            $Left: $Jease$.$InCubic,
-                            $Top: $Jease$.$InCubic,
-                            $Zoom: $Jease$.$InCubic,
-                            $Opacity: $Jease$.$OutQuad,
-                            $Rotate: $Jease$.$InCubic
-                        },
-                        $Opacity: 2,
-                        $Round: {$Rotate: 0.7}
-                    },
-                    {
-                        $Duration: 1200,
-                        x: 1,
-                        y: 2,
-                        $Cols: 2,
-                        $Zoom: 11,
-                        $Rotate: 1,
-                        $Assembly: 2049,
-                        $ChessMode: {$Column: 19},
-                        $Easing: {
-                            $Left: $Jease$.$InCubic,
-                            $Top: $Jease$.$InCubic,
-                            $Zoom: $Jease$.$InCubic,
-                            $Opacity: $Jease$.$OutQuad,
-                            $Rotate: $Jease$.$InCubic
-                        },
-                        $Opacity: 2,
-                        $Round: {$Rotate: 0.8}
+                    // },
+                    // {
+                    //     $Duration: 1000,
+                    //     $Zoom: 11,
+                    //     $SlideOut: true,
+                    //     $Easing: {$Zoom: $Jease$.$InExpo, $Opacity: $Jease$.$Linear},
+                    //     $Opacity: 2
+                    // },
+                    // {
+                    //     $Duration: 1200,
+                    //     $Zoom: 1,
+                    //     $Rotate: 1,
+                    //     $During: {$Zoom: [0.2, 0.8], $Rotate: [0.2, 0.8]},
+                    //     $Easing: {$Zoom: $Jease$.$Swing, $Opacity: $Jease$.$Linear, $Rotate: $Jease$.$Swing},
+                    //     $Opacity: 2,
+                    //     $Round: {$Rotate: 0.5}
+                    // },
+                    // {
+                    //     $Duration: 1000,
+                    //     $Zoom: 11,
+                    //     $Rotate: 1,
+                    //     $SlideOut: true,
+                    //     $Easing: {$Zoom: $Jease$.$InQuint, $Opacity: $Jease$.$Linear, $Rotate: $Jease$.$InQuint},
+                    //     $Opacity: 2,
+                    //     $Round: {$Rotate: 0.8}
+                    // },
+                    // {
+                    //     $Duration: 1200,
+                    //     x: 0.5,
+                    //     $Cols: 2,
+                    //     $Zoom: 1,
+                    //     $Assembly: 2049,
+                    //     $ChessMode: {$Column: 15},
+                    //     $Easing: {$Left: $Jease$.$InCubic, $Zoom: $Jease$.$InCubic, $Opacity: $Jease$.$Linear},
+                    //     $Opacity: 2
+                    // },
+                    // {
+                    //     $Duration: 1200,
+                    //     x: 4,
+                    //     $Cols: 2,
+                    //     $Zoom: 11,
+                    //     $SlideOut: true,
+                    //     $Assembly: 2049,
+                    //     $ChessMode: {$Column: 15},
+                    //     $Easing: {$Left: $Jease$.$InExpo, $Zoom: $Jease$.$InExpo, $Opacity: $Jease$.$Linear},
+                    //     $Opacity: 2
+                    // },
+                    // {
+                    //     $Duration: 1200,
+                    //     x: 0.6,
+                    //     $Zoom: 1,
+                    //     $Rotate: 1,
+                    //     $During: {$Left: [0.2, 0.8], $Zoom: [0.2, 0.8], $Rotate: [0.2, 0.8]},
+                    //     $Opacity: 2,
+                    //     $Round: {$Rotate: 0.5}
+                    // },
+                    // {
+                    //     $Duration: 1000,
+                    //     x: -4,
+                    //     $Zoom: 11,
+                    //     $Rotate: 1,
+                    //     $SlideOut: true,
+                    //     $Easing: {
+                    //         $Left: $Jease$.$InQuint,
+                    //         $Zoom: $Jease$.$InQuart,
+                    //         $Opacity: $Jease$.$Linear,
+                    //         $Rotate: $Jease$.$InQuint
+                    //     },
+                    //     $Opacity: 2,
+                    //     $Round: {$Rotate: 0.8}
+                    // },
+                    // {
+                    //     $Duration: 1200,
+                    //     x: -0.6,
+                    //     $Zoom: 1,
+                    //     $Rotate: 1,
+                    //     $During: {$Left: [0.2, 0.8], $Zoom: [0.2, 0.8], $Rotate: [0.2, 0.8]},
+                    //     $Opacity: 2,
+                    //     $Round: {$Rotate: 0.5}
+                    // },
+                    // {
+                    //     $Duration: 1000,
+                    //     x: 4,
+                    //     $Zoom: 11,
+                    //     $Rotate: 1,
+                    //     $SlideOut: true,
+                    //     $Easing: {
+                    //         $Left: $Jease$.$InQuint,
+                    //         $Zoom: $Jease$.$InQuart,
+                    //         $Opacity: $Jease$.$Linear,
+                    //         $Rotate: $Jease$.$InQuint
+                    //     },
+                    //     $Opacity: 2,
+                    //     $Round: {$Rotate: 0.8}
+                    // },
+                    // {
+                    //     $Duration: 1200,
+                    //     x: 0.5,
+                    //     y: 0.3,
+                    //     $Cols: 2,
+                    //     $Zoom: 1,
+                    //     $Rotate: 1,
+                    //     $Assembly: 2049,
+                    //     $ChessMode: {$Column: 15},
+                    //     $Easing: {
+                    //         $Left: $Jease$.$InCubic,
+                    //         $Top: $Jease$.$InCubic,
+                    //         $Zoom: $Jease$.$InCubic,
+                    //         $Opacity: $Jease$.$OutQuad,
+                    //         $Rotate: $Jease$.$InCubic
+                    //     },
+                    //     $Opacity: 2,
+                    //     $Round: {$Rotate: 0.7}
+                    // },
+                    // {
+                    //     $Duration: 1000,
+                    //     x: 0.5,
+                    //     y: 0.3,
+                    //     $Cols: 2,
+                    //     $Zoom: 1,
+                    //     $Rotate: 1,
+                    //     $SlideOut: true,
+                    //     $Assembly: 2049,
+                    //     $ChessMode: {$Column: 15},
+                    //     $Easing: {
+                    //         $Left: $Jease$.$InExpo,
+                    //         $Top: $Jease$.$InExpo,
+                    //         $Zoom: $Jease$.$InExpo,
+                    //         $Opacity: $Jease$.$Linear,
+                    //         $Rotate: $Jease$.$InExpo
+                    //     },
+                    //     $Opacity: 2,
+                    //     $Round: {$Rotate: 0.7}
+                    // },
+                    // {
+                    //     $Duration: 1200,
+                    //     x: -4,
+                    //     y: 2,
+                    //     $Rows: 2,
+                    //     $Zoom: 11,
+                    //     $Rotate: 1,
+                    //     $Assembly: 2049,
+                    //     $ChessMode: {$Row: 28},
+                    //     $Easing: {
+                    //         $Left: $Jease$.$InCubic,
+                    //         $Top: $Jease$.$InCubic,
+                    //         $Zoom: $Jease$.$InCubic,
+                    //         $Opacity: $Jease$.$OutQuad,
+                    //         $Rotate: $Jease$.$InCubic
+                    //     },
+                    //     $Opacity: 2,
+                    //     $Round: {$Rotate: 0.7}
+                    // },
+                    // {
+                    //     $Duration: 1200,
+                    //     x: 1,
+                    //     y: 2,
+                    //     $Cols: 2,
+                    //     $Zoom: 11,
+                    //     $Rotate: 1,
+                    //     $Assembly: 2049,
+                    //     $ChessMode: {$Column: 19},
+                    //     $Easing: {
+                    //         $Left: $Jease$.$InCubic,
+                    //         $Top: $Jease$.$InCubic,
+                    //         $Zoom: $Jease$.$InCubic,
+                    //         $Opacity: $Jease$.$OutQuad,
+                    //         $Rotate: $Jease$.$InCubic
+                    //     },
+                    //     $Opacity: 2,
+                    //     $Round: {$Rotate: 0.8}
                     }
                 ];
 
