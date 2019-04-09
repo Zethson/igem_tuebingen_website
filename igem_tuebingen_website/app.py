b8e5933047a412345040f3a78d0ca8944571609a
Imipenem <philipp_ehm@protonmail.com>
2019-04-09 20:18:37 +0200

    Again travis fix (ImportModuleError)

--- igem_tuebingen_website/app.py
+++ igem_tuebingen_website/app.py
@@ -1,8 +1,9 @@
 import logging
-import os
 import configparser
 from flask import Flask, request
 from flask_babel import Babel
+import os
+import click
 
 console = logging.StreamHandler()
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
@@ -46,6 +47,42 @@ except KeyError:
         MAIL_USERNAME="username_not_available",
         MAIL_PASSWORD="password_not_available"
     )
+
+
+@app.cli.group()
+def translate():
+    """Translation commands"""
+    pass
+
+
+@translate.command()
+@click.argument('lang')
+def init(lang):
+    """Initialize a new language"""
+    if os.system('pybabel extract -F babel.cfg -o messages.pot .'):
+        raise RuntimeError('extract command failed')
+    if os.system('pybabel init -i messages.pot -d igem_tuebingen_website/translations -l ' + lang):
+        raise RuntimeError('init command failed')
+    os.remove('messages.pot')
+
+
+@translate.command()
+def update():
+    """Update all languages"""
+
+    if os.system('pybabel extract -F babel.cfg -o messages.pot .'):
+        raise RuntimeError('extract command failed')
+    if os.system('pybabel update -i messages.pot -d igem_tuebingen_website/translations'):
+        raise RuntimeError('update command failed')
+    os.remove('messages.pot')
+
+
+@translate.command()
+def compile():
+    """Compile all languages"""
+
+    if os.system('pybabel compile -d igem_tuebingen_website/translations'):
+        raise RuntimeError('compile command failed')
 
 
 @babel.localeselector
