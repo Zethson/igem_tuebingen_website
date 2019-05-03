e1e54f64ac8f9a21bde55219745a9d89ad7974ab
Imipenem <philipp_ehm@protonmail.com>
2019-05-03 23:25:53 +0200

    [FEATURE] Anti-Spam Protection and async mail
    - added hidden forms for basic spam protection
    
    - sending a mail will now be handled in a separate Thread increasing speed

--- igem_tuebingen_website/handlers/contact_request.py
+++ igem_tuebingen_website/handlers/contact_request.py
@@ -1,13 +1,12 @@
 from flask import request, render_template
 from flask_mail import Mail
 from flask_mail import Message
+from threading import Thread
 from ..app import app
 
 
 @app.route('/contact_request', methods=['POST'])
 def contact_request():
-    mail = Mail(app)
-
     # validate form
     if not request.form['name'] or not request.form['email'] or not request.form['message'] or "@" not in request.form['email']:
         return render_template("contact_error.html")
@@ -18,5 +17,14 @@ def contact_request():
     sent_by = "The request was sent by: " + request.form['name'] + " with the contact e-mail: " + request.form['email'] + "\n\n"
     message = "His/her message is: \n\n" + request.form['message']
     msg.body = sent_by + message
-    mail.send(msg)
-    return render_template("contact_successful.html")
+    print(len(request.form['emailSPAM']))
+    if len(request.form['emailSPAM']) == 0:
+        Thread(target=send_async_email, args=(app, msg)).start()
+        return render_template("contact_successful.html")
+    return render_template("errors/410.html")
+
+
+def send_async_email(app, msg):
+    mail = Mail(app)
+    with app.app_context():
+        mail.send(msg)
---
+++ igem_tuebingen_website/static/assets/js/spam.js
@@ -0,0 +1,5 @@
+$(document).ready(
+function() {
+$('#emailSPAM').hide()
+}
+);
