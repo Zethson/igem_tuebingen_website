c2c724e08ee160324cc12b91450578550c53ebe9
eliasschreiner <eliasschreiner@gmx.de>
2020-04-12 23:00:42 +0200

    comments and fixes

--- igem_tuebingen_website/handlers/language.py
+++ igem_tuebingen_website/handlers/language.py
@@ -1,11 +1,11 @@
 from flask import redirect, url_for, session
 from ..app import app
 
-"""This route is requested, whenever (and only if) the user changed the language manually"""
+"""This route is requested, whenever (and only if) the user changed the language manually additionally a redirectURL can be added to not always land on index"""
 @app.route('/language/<language>')
 @app.route('/language/<language>/<redirectUrl>')
-def set_language(language=None, redirectUrl='home'):
+def set_language(language='en', redirectUrl='home'):
     session['language'] = language
-    print(redirectUrl)
-    print('Dasdasdasdasdasdassssssdsasfsadasdasdasf')
+    print('Language set to: ' + language)
+    print('Redirect URL: ' + redirectUrl)
     return redirect(url_for(redirectUrl))
