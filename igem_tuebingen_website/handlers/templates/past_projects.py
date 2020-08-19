7fc540cebf94a20cd089b9033709f1aa847975fc
eliasschreiner <eliasschreiner@gmx.de>
2020-08-19 11:18:32 +0200

    Past Projects content

--- igem_tuebingen_website/handlers/templates/past_projects.py
+++ igem_tuebingen_website/handlers/templates/past_projects.py
@@ -1,6 +1,6 @@
 from flask import render_template
 from igem_tuebingen_website.app import app
 
-@app.route('/past_projects')
-def past_projects():
-    return render_template('past_projects.html')
+@app.route('/past_projects/<year>')
+def past_projects(year=2020):
+    return render_template('past_projects.html', year=year)
