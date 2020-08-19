09330c6e2490027af0ec9814d81cea9772077846
eliasschreiner <eliasschreiner@gmx.de>
2020-08-19 12:03:37 +0200

    embedded sponsor logos

--- igem_tuebingen_website/handlers/templates/sponsor.py
+++ igem_tuebingen_website/handlers/templates/sponsor.py
@@ -1,6 +1,14 @@
 from flask import render_template
 from igem_tuebingen_website.app import app
+import os
 
 @app.route('/sponsor')
 def sponsor():
-    return render_template('sponsor.html')
+    sponsors = []
+    for filename in os.listdir('static/img/sponsors/2020'):
+        if filename.endswith(".png"):
+            sponsors.append(os.path.join('static/img/sponsors/2020/', filename))
+        else:
+            continue
+
+    return render_template('sponsor.html', sponsors=sponsors)
